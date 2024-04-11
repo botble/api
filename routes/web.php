75a785277c284999e918a2b6e0be771b25c6f488
Sang Nguyen <sangnguyenplus@gmail.com>
2024-04-11 10:20:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use Botble\Base\Facades\AdminHelper;
 use Illuminate\Support\Facades\Route;
 
 AdminHelper::registerRoutes(function () {
-    Route::name('api.')->group(function () {
+    Route::name('api.')->prefix('settings/api')->group(function () {
         Route::prefix('sanctum-token')->name('sanctum-token.')->group(function () {
             Route::resource('/', SanctumTokenController::class)
                 ->parameters(['' => 'sanctum-token'])
                 ->except('edit', 'update', 'show');
         });
 
-        Route::group(['prefix' => 'settings/api', 'permission' => 'api.settings'], function () {
+        Route::group(['permission' => 'api.settings'], function () {
             Route::get('/', [ApiController::class, 'edit'])->name('settings');
             Route::put('/', [ApiController::class, 'update'])->name('settings.update');
         });
