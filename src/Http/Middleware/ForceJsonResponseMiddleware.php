a33b9e111917e6d86ac800fdd04b338953c84506
Sang Nguyen <sangnguyenplus@gmail.com>
2024-12-08 10:24:40 +0700

    Update ForceJsonResponseMiddleware.php

--- src/Http/Middleware/ForceJsonResponseMiddleware.php
+++ src/Http/Middleware/ForceJsonResponseMiddleware.php
@@ -4,12 +4,17 @@ namespace Botble\Api\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ForceJsonResponseMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
+
+        if ($request->bearerToken()) {
+            Auth::setUser(Auth::guard('sanctum')->user());
+        }
 
         return $next($request);
     }
