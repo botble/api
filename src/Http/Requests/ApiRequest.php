498674a4cb538c18d6a21406c23c07cc449837f4
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:42:12 +0300

    Change namespace from Botble to Tiryaq in ApiRequest

--- src/Http/Requests/ApiRequest.php
+++ src/Http/Requests/ApiRequest.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Botble\Api\Http\Requests;
+namespace Tiryaq\Api\Http\Requests;
 
-use Botble\Base\Http\Responses\BaseHttpResponse;
+use Tiryaq\Base\Http\Responses\BaseHttpResponse;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Arr;
