f80044d55aac0b6c09bab32b1f6e13929e6303b9
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:41:26 +0300

    Change namespace from Botble to Tiryaq

--- src/Models/UserSetting.php
+++ src/Models/UserSetting.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Botble\Api\Models;
+namespace Tiryaq\Api\Models;
 
-use Botble\Base\Models\BaseModel;
-use Botble\Base\Models\Concerns\HasUuidsOrIntegerIds;
+use Tiryaq\Base\Models\BaseModel;
+use Tiryaq\Base\Models\Concerns\HasUuidsOrIntegerIds;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class UserSetting extends BaseModel
