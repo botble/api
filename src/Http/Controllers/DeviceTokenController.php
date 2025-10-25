96652bca4a7c825fb62d342a69339f46109f32f6
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:48:41 +0300

    Update DeviceTokenController.php

--- src/Http/Controllers/DeviceTokenController.php
+++ src/Http/Controllers/DeviceTokenController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Botble\Api\Http\Controllers;
+namespace Tiryaq\Api\Http\Controllers;
 
-use Botble\Api\Http\Requests\DeviceTokenRequest;
-use Botble\Api\Models\DeviceToken;
-use Botble\Base\Http\Responses\BaseHttpResponse;
+use Tiryaq\Api\Http\Requests\DeviceTokenRequest;
+use Tiryaq\Api\Models\DeviceToken;
+use Tiryaq\Base\Http\Responses\BaseHttpResponse;
 use Exception;
 use Illuminate\Http\Request;
 
