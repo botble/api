836d4005de69e5c0359b8bdd79fac3553acf53cd
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:51:00 +0300

    Update UserSettingSeeder.php

--- src/Database/Seeders/UserSettingSeeder.php
+++ src/Database/Seeders/UserSettingSeeder.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Botble\Api\Database\Seeders;
+namespace Tiryaq\Api\Database\Seeders;
 
-use Botble\Api\Models\UserSetting;
-use Botble\Base\Supports\BaseSeeder;
-use Botble\Ecommerce\Models\Customer;
+use Tiryaq\Api\Models\UserSetting;
+use Tiryaq\Base\Supports\BaseSeeder;
+use Tiryaq\Ecommerce\Models\Customer;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
