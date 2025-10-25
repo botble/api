740132c7ac7e659b2bf710069137b8d7c70417ac
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:51:13 +0300

    Update ProcessScheduledNotificationsCommand.php

--- src/Commands/ProcessScheduledNotificationsCommand.php
+++ src/Commands/ProcessScheduledNotificationsCommand.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Botble\Api\Commands;
+namespace Tiryaq\Api\Commands;
 
-use Botble\Api\Models\PushNotification;
-use Botble\Api\Services\PushNotificationService;
+use Tiryaq\Api\Models\PushNotification;
+use Tiryaq\Api\Services\PushNotificationService;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
