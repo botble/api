6322f2bc7251e65f201db805c415130c327fb78b
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:42:27 +0300

    Change namespace and imports in ApiSettingRequest

--- src/Http/Requests/ApiSettingRequest.php
+++ src/Http/Requests/ApiSettingRequest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Botble\Api\Http\Requests;
+namespace Tiryaq\Api\Http\Requests;
 
-use Botble\Base\Rules\OnOffRule;
-use Botble\Support\Http\Requests\Request;
+use Tiryaq\Base\Rules\OnOffRule;
+use Tiryaq\Support\Http\Requests\Request;
 
 class ApiSettingRequest extends Request
 {
